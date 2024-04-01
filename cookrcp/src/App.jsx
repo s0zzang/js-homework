b6e928ddf02132a4aabacc98ba1138a5a41de37c
s0zzang <dlth8888@gmail.com>
2024-04-01 23:43:24 +0900

    test. 재료별 데이터 통신 가능 여부 확인

--- cookrcp/src/App.jsx
+++ cookrcp/src/App.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
@@ -9,15 +10,58 @@ function App() {
       const res = await axios.get(
         `http://openapi.foodsafetykorea.go.kr/api/${
           import.meta.env.VITE_API_KEY
-        }/COOKRCP01/json/1/1000/RCP_PARTS_DTLS=소고기`,
+          // }/COOKRCP01/json/1/100/RCP_WAY2=굽기&RCP_NM=전`,
+          // }/COOKRCP01/json/1/1000/RCP_NM=라면`,
+          // }/COOKRCP01/json/1/1000/RCP_PARTS_DTLS=숙주`,
+          // }/COOKRCP01/json/1/100/RCP_WAY2=끓이기`,
+        }/COOKRCP01/json/1/1001`,
         {
           headers: {
             "content-type": "application/json",
             accept: "application/json",
           },
-        }
+        },
       );
       setData(res.data["COOKRCP01"].row);
+      // console.log(data);
+
+      // 국물 요리만 - 해장 느낌
+      // const processData = res.data["COOKRCP01"].row.filter(
+      //   rcp => rcp["RCP_WAY2"] === "끓이기" && rcp["RCP_PAT2"] === "국&찌개",
+      // );
+
+      // 비오는 날 : 전
+      // const processData = res.data["COOKRCP01"].row.filter(
+      //   rcp => rcp["RCP_WAY2"] === "굽기" && rcp["RCP_NM"].includes("전"),
+      // );
+
+      // 비오는 날 : 고기 구워
+      // const processData = res.data["COOKRCP01"].row.filter(
+      //   rcp => rcp["RCP_WAY2"] === "굽기" && rcp["RCP_NM"].includes("삼겹살"),
+      // );
+
+      // 비오는 날 : 라면
+      // const processData = res.data["COOKRCP01"].row.filter(
+      //   rcp => rcp["RCP_WAY2"] === "끓이기" && rcp["RCP_NM"].includes("라면"),
+      // );
+
+      // 너무 더워 : 냉파스타/오이냉국 등등 '냉' 붙은거 다보여줘
+      // const processData = res.data["COOKRCP01"].row.filter(
+      //   rcp =>
+      //     (rcp["RCP_NM"].includes("냉") || rcp["RCP_NM"].includes("초계")) &&
+      //     !rcp["RCP_NM"].includes("함초"),
+      // );
+
+      // 너무 더워 : 이열치열? 매워
+      const processData = res.data["COOKRCP01"].row.filter(rcp => rcp["RCP_NM"].includes("매운"));
+
+      // 눈 와 : 전골류
+      // const processData = res.data["COOKRCP01"].row.filter(rcp => rcp["RCP_NM"].includes("전골"));
+
+      // 날씨별 -> 해쨍쨍(시원한), 구름꼈어(뜨끈한 국물), 비와(삼겹살/전), 특색 없는 날엔 메뉴 키워드 랜덤 추천하기
+      // 불금 -> 해장국
+
+      setData(processData);
     } catch (err) {
       console.error(err);
     }
@@ -29,43 +73,57 @@ function App() {
 
   console.log(data);
 
-  const list = data?.map((item) => (
+  const list = data?.map(item => (
     <li key={item["RCP_SEQ"]}>
       <p>
-        작은 이미지 <img src={item["ATT_FILE_NO_MAIN"]} alt="" />
+        <div class="dis-no">작은 이미지</div> <img src={item["ATT_FILE_NO_MAIN"]} alt="" />
       </p>
-      <p>
+      {/* <p>
         큰 이미지 <img src={item["ATT_FILE_NO_MK"]} alt="" />
+      </p> */}
+      <div class="flex">
+        <p>
+          <strong>이름</strong> {item["RCP_NM"]}
+        </p>
+        <p>
+          <strong>카테고리</strong> {item["RCP_PAT2"]}
+        </p>
+        <p>
+          <strong>요리방법</strong> {item["RCP_WAY2"]}
+        </p>
+      </div>
+      <p>
+        <strong>재료</strong> {item["RCP_PARTS_DTLS"]}
       </p>
-      <p>이름: {item["RCP_NM"]}</p>
-      <p>카테고리: {item["RCP_PAT2"]}</p>
-      <p>요리방법: {item["RCP_WAY2"]}</p>
-      <p>재료: {item["RCP_PARTS_DTLS"]}</p>
-      <ul>
+      {/* <ul>
         <li>열량: {item["INFO_ENG"]}</li>
         <li>탄수화물: {item["INFO_CAR"]}</li>
         <li>단백질: {item["INFO_PRO"]}</li>
         <li>지방: {item["INFO_FAT"]}</li>
         <li>나트륨: {item["INFO_NA"]}</li>
-      </ul>
+      </ul> */}
       <ul>
         <li>
-          <img src={item["MANUAL_IMG01"]} alt="" />
+          {/* <img src={item["MANUAL_IMG01"]} alt="" /> */}
           {item["MANUAL01"]}
         </li>
         <li>
-          <img src={item["MANUAL_IMG02"]} alt="" />
+          {/* <img src={item["MANUAL_IMG02"]} alt="" /> */}
           {item["MANUAL02"]}
         </li>
         <li>
-          <img src={item["MANUAL_IMG03"]} alt="" />
+          {/* <img src={item["MANUAL_IMG03"]} alt="" /> */}
           {item["MANUAL03"]}
         </li>
+        <li> {item["MANUAL04"]} </li>
+        <li> {item["MANUAL05"]} </li>
+        <li> {item["MANUAL06"]} </li>
+        <li> {item["MANUAL07"]} </li>
       </ul>
     </li>
   ));
 
-  return <ul>{list}</ul>;
+  return <ul className="flex">{list}</ul>;
 }
 
 export default App;
